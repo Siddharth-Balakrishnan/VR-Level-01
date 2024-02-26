9caf590f09782d433dd89c49cb6cac790eae46eb
Siddharth-Balakrishnan <siddharthpersonal001@gmail.com>
2024-02-26 21:36:33 +0530

    Setup JumpScare

---
+++ VR Level 01/MindScape Oasis/Assets/Scripts/JumpScareTrigger.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class JumpScareTrigger : MonoBehaviour
+{
+     [SerializeField]
+     GameObject jumpScareObject; 
+     
+     [SerializeField]
+     float jumpScareDuration = 3f; 
+    
+    [SerializeField]
+    float destroyDelay = 5f; 
+
+     [SerializeField]
+     private bool triggered = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!triggered && other.CompareTag("Player"))
+        {
+            triggered = true;
+            StartCoroutine(DoJumpScare());
+        }
+    }
+
+    IEnumerator DoJumpScare()
+    {
+        if (jumpScareObject != null)
+        {
+            jumpScareObject.SetActive(true); 
+            yield return new WaitForSeconds(jumpScareDuration);
+            jumpScareObject.SetActive(false); 
+        }
+
+        yield return new WaitForSeconds(destroyDelay);
+        Destroy(gameObject);
+    }
+}
